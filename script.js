7ab978c0f335b616b8ab5cb9fcf56f09eb99d132
Lemonzer <177494093+lemonzer@users.noreply.github.com>
2025-06-24 19:35:38 -0700

    Input of 10 gives grid using nth-child 10n

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const container = document.querySelector('.container');
+
 const inputWidth = prompt("Enter width");
 
 
-
 for (let i = 0; i < (inputWidth * inputWidth) - inputWidth; i++) {
+  
     const box = document.createElement('div');
- 
     container.appendChild(box);
+  
+    
+
     box.addEventListener('mouseover', function() {
     this.style.backgroundColor = 'blue';
 });
