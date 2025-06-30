cd686d3782ea9edfc8507b9473fadd12f2c20afe
Lemonzer <177494093+lemonzer@users.noreply.github.com>
2025-06-29 17:49:44 -0700

    Set input to set value of width using nth-child

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ for (let i = 0; i < (inputWidth * inputWidth) - inputWidth; i++) {
   
     const box = document.createElement('div');
     container.appendChild(box);
-  
-    
+ 
+    const selected = document.querySelectorAll(`.container div:nth-child(${inputWidth}n)`);
+ 
+selected.forEach(item => {
+    selected.style.width = '100%';
+ });
 
     box.addEventListener('mouseover', function() {
     this.style.backgroundColor = 'blue';
-});
+    });
+
 }
-
-
